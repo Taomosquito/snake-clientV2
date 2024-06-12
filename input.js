fc2ee2e9782832037093fdc5e9c8b12cbc8a0e27
Taomosquito <S_Peloquin123@live.com>
2024-06-12 01:08:41 -0400

    refactored to use an input file

---
+++ input.js
@@ -0,0 +1,35 @@
+// setup interface to handle user input from stdin
+let connection;
+
+const setupInput = function (session) {
+  connection = session
+  const stdin = process.stdin;
+  stdin.setRawMode(true); 
+  stdin.setEncoding("utf8"); 
+  stdin.resume(); 
+  stdin.on("data", handleUserInput); 
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  // code handling the key press will go here
+  switch (key) {
+    case 'w':
+      connection.write("Move: up");
+      break;
+    case 's':
+      connection.write("Move: down");
+      break;
+    case 'a':
+      connection.write("Move: left");
+      break;
+    case 'd':
+      connection.write("Move: right");
+      break;
+    case '\u0003':
+      process.exit();
+      break;
+  }
+};
+
+module.exports = { setupInput };
--- play.js
+++ play.js
@@ -1,39 +1,6 @@
-const { session } = require("./client.js");
-console.log(`Connecting`)
-// console.log(`Connecting to ${connectionHost}:${connectionPort}}`);
+const { session } = require("./client");
+const { setupInput } = require("./input");
 
-// setup interface to handle user input from stdin
-let connection;
-
-const setupInput = function (session) {
-  connection = session
-  const stdin = process.stdin;
-  stdin.setRawMode(true); 
-  stdin.setEncoding("utf8"); 
-  stdin.resume(); 
-  stdin.on("data", handleUserInput); 
-  return stdin;
-};
-
-const handleUserInput = function (key) {
-  // code handling the key press will go here
-  switch (key) {
-    case 'w':
-      connection.write("Move: up");
-      break;
-    case 's':
-      connection.write("Move: down");
-      break;
-    case 'a':
-      connection.write("Move: left");
-      break;
-    case 'd':
-      connection.write("Move: right");
-      break;
-    case '\u0003':
-      process.exit();
-      break;
-  }
-};
-
-setupInput(session());
+console.log("Connecting ...");
+const connection = session();
+setupInput(connection);
